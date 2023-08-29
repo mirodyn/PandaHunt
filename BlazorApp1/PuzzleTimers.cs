454e15261051efa2dbadfc09afabfc3b2311b62b
Jirka Smutný <jiri.smutny@solidvision.cz>
2023-08-29 08:55:32 +0200

    Add project files.

---
+++ BlazorApp1/PuzzleTimers.cs
@@ -0,0 +1,17 @@
+﻿using PandaHunt.Pages;
+
+internal class PuzzleTimers
+{
+
+    public bool PuzzleAOpened => DateTime.Now > PuzzleATime;
+    public bool PuzzleBOpened => DateTime.Now > PuzzleBTime;
+    public bool PuzzleCOpened => DateTime.Now > PuzzleCTime;
+    public bool PuzzleDOpened => DateTime.Now > PuzzleDTime;
+
+
+    public DateTime PuzzleATime = new DateTime(2023, 9, 1).AddHours(16);
+    public DateTime PuzzleBTime = new DateTime(2023, 8, 1).AddHours(16);
+    public DateTime PuzzleCTime = new DateTime(2023, 9, 1).AddHours(16);
+    public DateTime PuzzleDTime = new DateTime(2023, 9, 1).AddHours(16);
+
+}
---
+++ BlazorApp1/wwwroot/download.js
@@ -0,0 +1,19 @@
+﻿// wwwroot/download.js
+function downloadFile(base64Content,name) {
+    const byteCharacters = atob(base64Content);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+
+    const blob = new Blob([byteArray], { type: 'image/png' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = name;
+    document.body.appendChild(a);
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
