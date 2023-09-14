76876aa1cbdcfc40984e394cb080ffdc77b972e2
Jirka Smutný <jiri.smutny@solidvision.cz>
2023-09-14 12:52:07 +0200

    timers set

--- BlazorApp1/PuzzleTimers.cs
+++ BlazorApp1/PuzzleTimers.cs
@@ -14,4 +14,9 @@ internal class PuzzleTimers
     public DateTime PuzzleCTime = new DateTime(2023, 9, 20).AddHours(12);
     public DateTime PuzzleDTime = new DateTime(2023, 9, 24).AddHours(12);
 
+    //public DateTime PuzzleATime = new DateTime(2023, 8, 13).AddHours(12);
+    //public DateTime PuzzleBTime = new DateTime(2023, 8, 17).AddHours(12);
+    //public DateTime PuzzleCTime = new DateTime(2023, 8, 20).AddHours(12);
+    //public DateTime PuzzleDTime = new DateTime(2023, 8, 24).AddHours(12);
+
 }
